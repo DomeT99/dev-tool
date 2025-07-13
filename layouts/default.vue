8259806c4099d92077d2e540ad2b48738cf5c49d
Domenico Tenace <domenicotenace99@gmail.com>
2025-07-13 17:35:14 +0200

    feat: add initial layout and components for the application

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+ <template>
+  <div class="min-h-screen bg-background font-sans">
+    <CommonNavbar />
+    <main class="container mx-auto py-8">
+      <slot />
+    </main>
+  </div>
+</template>
